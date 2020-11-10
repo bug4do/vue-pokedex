0de6cf89baa786b46fd268f77e333f148b9b8908
Bugado <addminplayerbr1122@gmail.com>
2020-11-10 02:14:51 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <h1 class="title is-3">Pokedex</h1>
+      <input type="text" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded">
+      <hr>
+      <div v-for="(poke, index) in buscaResult" :key="index" class="list">
+          <Pokemon :id="index" :name="poke.name" :url="poke.url"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data() {
+    return {
+      busca: '',
+      pokemons: []
+    }
+  },
+  computed: {
+    buscaResult: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        console.log(this.pokemons.filter(pokemon => pokemon.name == this.busca));
+        return this.pokemons.filter(pokemon => pokemon.name.toLowerCase() == this.busca.toLowerCase());
+      }
+    }
+  },
+  created: function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=251&offset=0').then(res => {
+      this.pokemons = res.data.results;
+    });
   }
 }
+
 </script>
 
 <style>
